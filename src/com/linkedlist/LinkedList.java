a5f7c40c6b2a306dbf3e9fbb1981dbd83d537029
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-10-09 11:12:53 +0530

    [Dipali] Added : create linked list

---
+++ src/com/linkedlist/LinkedList.java
@@ -0,0 +1,34 @@
+package com.linkedlist;
+/**
+ * 
+ * @author Dipal
+ *
+ */
+public class LinkedList {
+	Node head;
+
+	class Node {
+		Object data;
+		Node ref;
+
+		Node(Object data) {
+			this.data = data;
+		}
+	}
+
+	public void elements(Object data) {
+		Node newNode = new Node(data);
+		System.out.print(newNode.data + " ");
+	}
+
+	public static void main(String[] args) {
+		LinkedList list = new LinkedList();
+		System.out.println("Welcome to LinkedList program");
+		list.elements(56);
+		list.elements(30);
+		list.elements(70);
+	}
+
+}
+
+
